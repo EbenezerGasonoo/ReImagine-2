96e04494ecfc52b2cb8221e7cb3854329ee0046d
Ebenezer Gasonoo <iamknightrae@gmail.com>
2018-08-14 05:20:49 +0100

    INitial

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+"use strict";
+
+	/*------------- preloader js --------------*/
+	$(window).load(function() { // makes sure the whole site is loaded
+		$('.loader-container').fadeOut(); // will first fade out the loading animation
+		$('.loader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
+		$('body').delay(150).css({'overflow':'visible'})
+	})
+
+	// slicknav
+	$('ul#navigation').slicknav({
+		prependTo:".responsive-menu-wrap"
+	});
+	
+	/*--
+		Scroll Up
+	------------------------*/
+	$.scrollUp({
+		easingType: 'linear',
+		scrollSpeed: 900,
+		animation: 'fade',
+		scrollText: '<i class="fa fa-chevron-up"></i>',
+	});
+
+})(jQuery);	
